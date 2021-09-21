55cbe7ee5f0da9f57558118bed2f33f4461cc655
Gabriel Dietrich Guesser <gdguesser@gmail.com>
2021-09-21 03:37:33 +0000

    Initial commit

---
+++ src/test/kotlin/micronaut/br/com/MicronautapiTest.kt
@@ -0,0 +1,19 @@
+package micronaut.br.com
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class MicronautapiTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
